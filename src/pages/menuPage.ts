16a19d6fcf3bfc80040a32bd00c7f6e425b22c5f
ALi Salama <alysaied2000@gmail.com>
2026-09-25 04:22:35 +0300

    structure menupage and style it and add photos

--- src/pages/menuPage.ts
+++ src/pages/menuPage.ts
@@ -1,12 +1,58 @@
+import menu1 from "../assets/menu1.png"
+import menu2 from "../assets/menu2.png"
+import menu3 from "../assets/menu3.png"
+import menu4 from "../assets/menu4.png"
+
 const MenuPage = () => {
-    const menuContainer = document.createElement("div") as HTMLDivElement
-    menuContainer.classList.add("menu-container")
+  const menuContainer = document.createElement("div") as HTMLDivElement;
+  menuContainer.classList.add("menu-container");
 
-    menuContainer.innerHTML = `
-        <h1>Menu</h1>
-    `
+  menuContainer.innerHTML = `
+       <div class="cards">
+            <div class="card">
+                <div class="img">
+                    <img src=${menu1} class="img-card-menu"/>
+                </div>
+                <div class="details">
+                    <p>Firecracker Chicken</p>
+                    <p class="price">$ 99.00</p>
+                    <button class="btn-menu-card">More</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src=${menu2} class="img-card-menu"/>
+                </div>
+                <div class="details">
+                    <p>Firecracker Chicken</p>
+                    <p class="price">$ 99.00</p>
+                    <button class="btn-menu-card">More</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src=${menu3} class="img-card-menu"/>
+                </div>
+                <div class="details">
+                    <p class="price">Firecracker Chicken</p>
+                    <p>$ 99.00</p>
+                    <button class="btn-menu-card">More</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src=${menu4} class="img-card-menu"/>
+                </div>
+                <div class="details">
+                    <p>Firecracker Chicken</p>
+                    <p class="price">$ 99.00</p>
+                    <button class="btn-menu-card">More</button>
+                </div>
+            </div>
+       </div>
+    `;
 
-    return menuContainer
-}
+  return menuContainer;
+};
 
-export default MenuPage
+export default MenuPage;
