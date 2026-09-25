b95bf9217ffd9e293c1f6d13e609ed7375ea8414
ALi Salama <alysaied2000@gmail.com>
2026-09-25 05:02:27 +0300

    add structure of about page and style it

--- src/pages/aboutPage.ts
+++ src/pages/aboutPage.ts
@@ -1,9 +1,45 @@
+import bgAboutImg from "../assets/bgAbout.png";
+
 const AboutPage = () => {
     const aboutContainer = document.createElement("div")
     aboutContainer.classList.add("about-container")
 
     aboutContainer.innerHTML = `
-        <h1>About</h1>
+        <div class="left-about">
+            <div class="row">
+                <div class="icon">
+                    <i class="fa-solid fa-phone phone"></i>
+                </div>
+                <div class="info">                
+                    <h3>Call Us</h3>
+                    <p>+1 (123) 456-7890</p>
+                    <p>Available: Mon–Sun | 10:00 AM – 10:00 PM</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="icon">
+                    <i class="fa-solid fa-location-dot location"></i>
+                </div>
+                <div class="info">                
+                    <h3>Visit Us</h3>
+                    <p>BurgerHub</p>
+                    <p>123 Flavor Street,Burger City, BC 45678</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="icon">
+                    <i class="fa-solid fa-envelope emailus"></i>
+                </div>
+                <div class="info">
+                    <h3>Email Us</h3>
+                    <p>hello@burgerhub.com</p>
+                    <p>We usually respond within 24 hours</p>
+                </div>
+            </div>
+        </div>
+        <div class="right-about">
+            <img src=${bgAboutImg} />
+        </div>
     `
 
     return aboutContainer
